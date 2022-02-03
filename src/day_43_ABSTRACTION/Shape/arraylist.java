fc2a3904f093d321d84f8912cd2d5e3d79210753
ozgechengo <93153305+ozgechengo@users.noreply.github.com>
2022-02-03 09:21:08 -0500

    Butun dosyalar 6

--- src/day_43_ABSTRACTION/Shape/arraylist.java
+++ src/day_43_ABSTRACTION/Shape/arraylist.java
@@ -4,18 +4,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class arraylist {
+
     public static void main(String[] args) {
 
+        Circle circle = new Circle(4.5);
+        Square square = new Square(5);
+        Rectangle rectangle = new Rectangle(4, 8);
 
-    Circle circle = new Circle(1);
-    Rectangle rectangle = new Rectangle(3, 4);
-    Square square = new Square(2);
+        ArrayList<Shape> listOFShapes = new ArrayList<>(Arrays.asList(circle, square, rectangle));
 
+        // System.out.println("listOFShapes = " + listOFShapes);
 
-    ArrayList<Shape>list=new ArrayList<>(Arrays.asList(square,circle,rectangle));
+        for (Shape each : listOFShapes) {
+            System.out.println("Area of " + each.getName() + " " + each.area());
+            System.out.println("Perimeter of " + each.getName() + " " + each.perimeter());
+        }
 
+        double min = Double.MAX_VALUE;
+        for (Shape each : listOFShapes) {
+            if (min > each.area()) {
+                min = each.area();
+            }
+        }
+        System.out.println("min = " + min);
 
-
-
-
-}}
+    }
+}
